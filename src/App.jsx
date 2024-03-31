c99e51f8aca4596bc13f1aed20f8128635ed7fd1
Balram Uddhav Melkunde <102988461+BalramMEL@users.noreply.github.com>
2024-03-31 16:11:53 +0530

    weather

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import Main from "./components/Main";
+
+
+export default function App() {
+  const [city, setCity] = useState("");
+
+  const handleSearch = (c) => {
+    setCity(c);
+  };
+
+  return (
+    <div className="min-h-svh flex flex-col justify-center items-center text-white bg-[url('/src/assets/images/bg.svg')] bg-cover bg-center bg-bg">
+      <Header onSearch={handleSearch} />
+      <Main city={city} />      
+    </div>
+  );
+}
